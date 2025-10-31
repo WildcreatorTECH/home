660bdc881ffae751b4afed2655fb583c6d43da6f
Wildcreator <142033327+Wildcreator2010@users.noreply.github.com>
2025-10-31 19:01:29 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -110,7 +110,7 @@ onMounted(() => {
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
   const title1 = "Wildcreator的主页";
-  const title2 = `hello world！`;
+  const title2 = `Hello World！`;
   const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
 });
